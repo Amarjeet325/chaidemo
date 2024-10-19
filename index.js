38b446282bc3a9cff9e393b9066afda7642940ac
Amarjeet <amarjeetpatel25112002@gmail.com>
2024-10-19 23:58:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//  console.log("chai aur code"
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/login', (req, res) => {
+    res.send('Login page!')
+  })
+  app.get('/code', (req, res) => {
+    res.send('Hello  chai aur code')
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
